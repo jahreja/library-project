113840dca0939cf5ba9182a588a1bf375d0b48de
Jack Taylor <77901413+jahreja@users.noreply.github.com>
2021-02-24 19:14:13 +0000

    more ui updates

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -6,8 +6,8 @@ FilePond.registerPlugin(
 
 FilePond.setOptions({
     stylePanelAspectRatio: 300 / 300,
-    imageResizeTargetWidth: 300,
-    imageResizeTargetHeight: 300
+    imageResizeTargetWidth: 500,
+    imageResizeTargetHeight: 500
 })
 
 FilePond.parse(document.body);
